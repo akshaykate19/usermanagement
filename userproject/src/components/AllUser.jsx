21951a6498e3a92bf9c5b91b577d16051171b973
Akshay Kate <akkshaykate@gmail.com>
2025-12-01 10:35:01 +0000

    User3

---
+++ userproject/src/components/AllUser.jsx
@@ -0,0 +1,55 @@
+import users from "./User";
+import SingleUser from "./SingleUser";
+import { useState } from "react";
+
+const AllUsers=()=>
+{
+    const [emp,setEmp]=useState([]);
+    const [showemp,setShowemp]=useState(false);
+
+    const addEmp=()=>{
+        setEmp(users);
+        setShowemp(true);
+    }
+
+    const deleteAllUser=()=>{
+        setEmp([]);
+        setShowemp(false);
+    }
+
+    const deleteUser=(id)=>{
+        const updatedEmp=emp.filter(e=>e.id!==id);
+        setEmp(updatedEmp);
+        if(updatedEmp.length===0)
+        {
+            setShowemp(false);
+        }
+    }
+
+    return(
+        <>
+            <div>
+                <h1>Employee Data</h1>
+                {!showemp && (
+                    <button onClick={addEmp}>Add Emp</button>
+                )}
+
+                {showemp &&(
+                <table border={1} cellPadding={10} cellSpacing={10}>
+                    <tbody>
+                        {emp.map(e=>(
+                            <SingleUser key={e.id} user={e} deleteUser={deleteUser}/>
+                        ))}
+                    </tbody>
+                </table>
+                )}
+
+                {showemp && (
+                    <button onClick={deleteAllUser}> Delete All Users</button>
+                )}
+            </div>
+        </>
+    )
+}
+
+export default AllUsers;
